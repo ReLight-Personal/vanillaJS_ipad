57da2854a9d26469fd49d2a50cb97d85fe84f606
ReLight-Labtop <yjkwang89727@gmail.com>
2023-12-31 18:12:15 +0900

    중랑 스터디 이후 작업까지

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import ipads from '../data/ipads.js';
+
 // 장바구니 - Basket
 const basketStartEl = document.querySelector("header .basket-starter");
 const basketEl = basketStartEl.querySelector(".basket");
@@ -95,4 +97,30 @@ pauseBtn.addEventListener("click", () => {
   video.pause();
   playBtn.classList.remove("hide");
   pauseBtn.classList.add("hide");
+});
+
+// '당신에게 맞는 iPad는?' 렌더링!
+const itemsEl = document.querySelector('section.compare .items');
+ipads.forEach((ipad) => {
+  const itemEl = document.createElement('div');
+  itemEl.classList.add('item');
+
+  let colorList = '';
+  ipad.colors.forEach((color) => {
+    colorList += /*html*/`<li style='background-color: ${color};'></li>`
+  });
+
+  itemEl.innerHTML = /* html */ `
+    <div class='thumbnail'>
+      <img src='${ipad.thumbnail}' alt='${ipad.name}' />
+    </div>
+    <ul class='colors'>${colorList}</ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class='tagline'>${ipad.tagline}</p>
+    <p class='price'>￦${ipad.price.toLocaleString('en-US')}</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+  `;
+
+  itemsEl.append(itemEl);
 });
